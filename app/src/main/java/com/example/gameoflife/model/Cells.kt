38be24de1f489824c86e4c4dab11412e5960dc59
HexedAgain <gregory.astley@gmail.com>
2023-12-14 23:39:23 +0000

    get some liveness tests for noddy implementation

--- app/src/main/java/com/example/gameoflife/model/Cells.kt
+++ app/src/main/java/com/example/gameoflife/model/Cells.kt
@@ -6,12 +6,27 @@ class Cells(
     private val _grid: MutableList<MutableList<Boolean>> = grid.map { it.toMutableList() }.toMutableList()
     fun get(): List<List<Boolean>> = _grid
 
-    fun makeCellLive(x: Int, y: Int): Boolean {
-        return setLiveness(x, y, alive = true)
+    val totalCells: Int = get().flatten().size
+
+    fun makeCellLive(row: Int, column: Int): Boolean {
+        return setLiveness(row, column, alive = true)
     }
 
-    fun makeCellDead(x: Int, y: Int): Boolean {
-        return setLiveness(x, y, alive = false)
+    fun makeCellDead(row: Int, column: Int): Boolean {
+        return setLiveness(row, column, alive = false)
+    }
+
+    fun getNoOfLiveNeighbours(row: Int, column: Int): Int {
+        var sum = 0
+        for (i in (row - 1)..(row + 1)) {
+            for (j in (column - 1)..(column) + 1) {
+                if (i == row && j == column) continue // don't include target cell liveness in sum
+                if (i >= 0 && i < _grid.size && j >= 0 && j < _grid[i].size) {
+                    if (_grid[i][j]) sum += 1
+                }
+            }
+        }
+        return sum
     }
 
     private fun setLiveness(x: Int, y: Int, alive: Boolean): Boolean {
--- app/src/test/java/com/example/gameoflife/model/CellsTest.kt
+++ app/src/test/java/com/example/gameoflife/model/CellsTest.kt
@@ -1,15 +1,32 @@
 package com.example.gameoflife.model
 
+import io.kotest.assertions.AssertionFailedError
 import io.kotest.core.spec.style.FreeSpec
 import io.kotest.matchers.shouldBe
 
 class CellsTest: FreeSpec() {
     lateinit var cells: Cells
-//    fun setup(grid: List<List<Boolean>> = (0 until 16).map { false}.chunked(4)) {
-//        cells = Cells(grid)
-//    }
     fun setup(rows: Int = 4, columns: Int = 4) {
         cells = Cells.makeGrid(rows, columns)
+    }
+
+    fun testGrid(cells: Cells, action: (Cells, Int, Int) -> Unit) {
+        (0 until cells.totalCells).forEach { idx ->
+            val row = idx / 3;
+            val column = idx % 3;
+            try {
+                action(cells, row, column)
+            } catch (ex: AssertionFailedError) {
+                throw AssertionFailedError(
+                    message ="""test failed at row: $row, column: $column
+                                   |    expected: ${ex.expectedValue}, actual: ${ex.actualValue}
+                               """.trimMargin(),
+                    cause = ex.cause,
+                    expectedValue = ex.expectedValue,
+                    actualValue = ex.actualValue
+                )
+            }
+        }
     }
     init {
         "makeGrid" - {
@@ -78,24 +95,77 @@ class CellsTest: FreeSpec() {
                 }
             }
         }
-        "getCellLiveness" - {
-            "if a cell has zero or one live neighbours the cell dies of starvation" - {
-                "zero neighbours" - {
-                    "cell on corner" {
-                        setup(rows = 2, columns = 2)
-                        true shouldBe false
-                    }
-                    "cell on edge" {
-                        true shouldBe false
-                    }
-                    "cell inside" {
-                        true shouldBe false
+        "getNoOfLiveNeighbours" - {
+            "it returns the count of cells that are one cell away from target in any direction" - {
+                "0 live" {
+                    cells = Cells.makeGrid(3, 3)
+
+                    testGrid(cells) { cells, row, column ->
+                        cells.getNoOfLiveNeighbours(row, column) shouldBe 0
                     }
                 }
-                "one neighbour" {
+                // Note can exploit rotational symmetry to exhaustively test these ...
+                "1 live" - {
+                    "live at corner" {
+                        cells = Cells.makeGrid(3, 3)
+                        cells.makeCellLive(0, 0)
 
+                        val expected = listOf(
+                            0,1,0,
+                            1,1,0,
+                            0,0,0).chunked(3)
+
+                        testGrid(cells) { cells, row, column ->
+                            cells.getNoOfLiveNeighbours(row, column) shouldBe expected[row][column]
+                        }
+                    }
+                    "live at edge" {
+                        cells = Cells.makeGrid(3, 3)
+                        cells.makeCellLive(0, 1)
+
+                        val expected = listOf(
+                            1,0,1,
+                            1,1,1,
+                            0,0,0).chunked(3)
+
+                        testGrid(cells) { cells, row, column ->
+                            cells.getNoOfLiveNeighbours(row, column) shouldBe expected[row][column]
+                        }
+                    }
+                    "live inside" {
+                        cells = Cells.makeGrid(3, 3)
+                        cells.makeCellLive(1, 1)
+
+                        val expected = listOf(
+                            1,1,1,
+                            1,0,1,
+                            1,1,1).chunked(3)
+
+                        testGrid(cells) { cells, row, column ->
+                            cells.getNoOfLiveNeighbours(row, column) shouldBe expected[row][column]
+                        }
+                    }
                 }
             }
         }
+//        "getCellLiveness" - {
+//            "if a cell has zero or one live neighbours the cell dies of starvation" - {
+//                "zero neighbours" - {
+//                    "cell on corner" {
+//                        setup(rows = 2, columns = 2)
+//                        true shouldBe false
+//                    }
+//                    "cell on edge" {
+//                        true shouldBe false
+//                    }
+//                    "cell inside" {
+//                        true shouldBe false
+//                    }
+//                }
+//                "one neighbour" {
+//                    true shouldBe false
+//                }
+//            }
+//        }
     }
 }
