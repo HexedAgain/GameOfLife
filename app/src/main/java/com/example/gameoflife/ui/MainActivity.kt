d181c6d8268304ee44fe198952cd1413aef5fd81
HexedAgain <gregory.astley@gmail.com>
2023-12-24 23:25:43 +0000

    rudimentary dragging functionality

--- app/src/main/java/com/example/gameoflife/ui/MainActivity.kt
+++ app/src/main/java/com/example/gameoflife/ui/MainActivity.kt
@@ -1,15 +1,16 @@
 package com.example.gameoflife.ui
 
-import android.content.Context
 import android.os.Bundle
-import android.util.DisplayMetrics
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.gestures.draggable2D
+import androidx.compose.foundation.gestures.rememberDraggable2DState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -43,6 +44,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
@@ -51,9 +53,9 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.zIndex
 import com.example.gameoflife.R
 import com.example.gameoflife.ui.ActionButtonConfig.ActionType
 import com.example.gameoflife.ui.theme.GameOfLifeTheme
@@ -223,21 +225,11 @@ fun NumberOfGenerationsRow(
     }
 }
 
-fun Float.toDp(context: Context): Dp {
-    val density = (context.resources.displayMetrics.densityDpi.toFloat() / DisplayMetrics.DENSITY_DEFAULT)
-    return (this / density).dp
-}
-
-fun Dp.toPx(context: Context): Float {
-    val density = (context.resources.displayMetrics.densityDpi.toFloat() / DisplayMetrics.DENSITY_DEFAULT)
-    return (this.value * density)
-}
-
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun CellsGrid(
     mainScreenViewModel: MainScreenViewModel = koinViewModel()
 ) {
-    // FIXME - this is broken again :(
     val cells by mainScreenViewModel.cells.collectAsState()
     val numRows = cells.get().size
     val numCols = cells.get().firstOrNull()?.size ?: 0
@@ -246,25 +238,48 @@ fun CellsGrid(
     val gridHeight = configuration.screenHeightDp.dp - 128.dp
     val width = gridWidth / numCols
     val height = gridHeight / numRows
-    val context = LocalContext.current
+    val dragStart = remember { mutableStateOf(Offset(Float.MAX_VALUE, Float.MAX_VALUE)) }
+    val dragEnd = remember { mutableStateOf(Offset(Float.MAX_VALUE, Float.MAX_VALUE)) }
+    val draggable2DState = rememberDraggable2DState(onDelta = {
+        dragEnd.value = Offset(dragEnd.value.x + it.x, dragEnd.value.y + it.y)
+    })
+
+    val selectedStart = Pair(-1, -1)
+    val selectedEnd = Pair(-1, -1)
     Column(
         // Note: defining clickable on each of the cells seems to be a massive performance hit
         modifier = Modifier
             .pointerInput(Pair(numRows, numCols)) {
                 detectTapGestures(
                     onTap = {
-                        val offsetX = it.x.toDp(context) - 0.dp
-                        val offsetY = it.y.toDp(context) - 0.dp
+                        val offsetX = it.x.toDp() - 0.dp
+                        val offsetY = it.y.toDp() - 0.dp
                         val columnIdx = (offsetX / width).toInt()
                         val rowIdx = (offsetY / height).toInt()
                         mainScreenViewModel.updateCell(row = rowIdx, column = columnIdx)
-                    }
+                    },
                 )
             }
+            .draggable2D(
+                draggable2DState,
+                onDragStarted = {
+                    dragStart.value = it
+                    dragEnd.value = Offset(0f, 0f)
+                }
+            )
             .height(gridHeight)
             .width(gridWidth)
             .border(border = BorderStroke(1.dp, Color.Gray))
     ) {
+        if (dragStart.value.x < Float.MAX_VALUE && dragStart.value.y < Float.MAX_VALUE && dragEnd.value.x < Float.MAX_VALUE && dragEnd.value.y < Float.MAX_VALUE) {
+            Canvas(modifier = Modifier.zIndex(1f)) {
+                drawRect(
+                    topLeft = dragStart.value,
+                    color = Color.Yellow.copy(alpha = 0.5f),
+                    size = Size(dragEnd.value.x, dragEnd.value.y)
+                )
+            }
+        }
         val cellModifier = Modifier
             .width(width)
             .height(height)
